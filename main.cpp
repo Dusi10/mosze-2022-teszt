068f60d3d2145486c2f1857f3114d9b892ec4677
Végh Ádám <97345798+Dusi10@users.noreply.github.com>
2022-09-10 14:41:13 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int *b = new int[NELEMENTS]; //NELEMENTS helyet N_ELEMENTS kell
     std::cout << '1-100 ertekek duplazasa' //'' helyett "" kell mivel ez egy string, illetve ; hiányzik a végére
-    for (int i = 0;) //for ciklus nem megfelelő, 
+    for (int i = 0;) //for ciklus nem megfelelő, std::endl hiányzik
     {
         b[i] = i * 2;
     }
@@ -18,7 +18,7 @@ int main()
     int atlag;
     for (int i = 0; i < N_ELEMENTS, i++) // , helyett ; kell
     {
-        atlag += b[i]
+        atlag += b[i] // ; a végére
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
