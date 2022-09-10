28bcea2be7f8a6fe7a1f1bdd3953402c7e280828
Végh Ádám <97345798+Dusi10@users.noreply.github.com>
2022-09-10 15:21:56 +0200

    Update main.cpp
    Hibák javítása

--- main.cpp
+++ main.cpp
@@ -4,21 +4,18 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS]; // NELEMENTS helyet N_ELEMENTS hibás megnevezés miatt
-    std::cout << '1-100 ertekek duplazasa' //'' helyett "" kell mivel ez egy string, illetve ; hiányzik a végére egy ;
-    for (int i = 0;) //for ciklus nem megfelelő
+    int *b = new int[N_ELEMENTS]; // NELEMENTS helyet N_ELEMENTS hibás megnevezés miatt
+    std::cout << "1-100 ertekek duplazasa" << std::endl; //'' helyett "" kell mivel ez egy string, illetve ; hiányzik a végére egy ;
+    for (int i = 1; i <= N_ELEMENTS; i++) //for ciklus nem megfelelő
     {
         b[i] = i * 2;
-    }
-    for (int i = 0; i; i++)
-    {
-        std::cout << "Ertek:" //pontosvessző hiányzik
-    }    
+        std::cout << "Ertek:" << b[i] << std::endl; //pontosvessző hiányzik
+    }   
     std::cout << "Atlag szamitasa: " << std::endl; 
     int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++) // , helyett ; kell
+    for (int i = 1; i <= N_ELEMENTS; i++) // , helyett ; kell, illetve megengedjük az egyenlőséget
     {
-        atlag += b[i] // ; a végére
+        atlag += b[i]; // ; a végére
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
